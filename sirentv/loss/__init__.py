2c36dbfe9eb7ae129c8e0da99525846fe964367b
seanxia8 <seanxia8@gmail.com>
2026-09-11 14:48:34 -0700

    added new loss functions

--- sirentv/loss/__init__.py
+++ sirentv/loss/__init__.py
@@ -1,6 +1,8 @@
 from .mse import WeightedL2Loss, L2Loss, UncertainMSE, VisibilityGradient_L2Loss
 from .cosine import WeightedCosineDissimilarity
 from .poisson import WeightedPoissonNLLLoss
+from .quantile_recon import WeightedReconstructedQuantileLoss
+from .grad_frob import WeightedGradFrobLoss
 from .reg import L2Regularization, L1Regularization, LNRegularization
 from .js import JSDivergenceLoss
 from .smooth_l1 import WeightedSmoothL1Loss, SmoothL1Loss, VisibilityGradient_SmoothL1Loss
